12b08fbc891df86dec539354ee4fa18e08f7307f
homp6 <homp@unitec.edu>
2025-03-17 17:27:19 -0600

    Primer commit de react

---
+++ gremio_hechiceros/src/Components/components.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function component(nombre){
+    return (
+        <button>
+            Boton, {nombre}
+        </button>
+    )
+}
+
+export default component;
